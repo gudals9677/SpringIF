8c135e496dce6c5fddbcfd35c824f99b3130519b
gudals <gudals9677@gmail.com>
2024-04-05 17:44:58 +0900

    123

--- login/src/main/java/hello/login/domain/member/MemberRepository.java
+++ login/src/main/java/hello/login/domain/member/MemberRepository.java
@@ -10,11 +10,11 @@ import java.util.*;
 public class MemberRepository {
 
     private static Map<Long, Member> store = new HashMap<>(); //static 사용
-    private static long sequence = 0L; //static 사용
+    private static long sequence = 0L;
 
     public Member save(Member member){
         member.setId(++sequence);
-        log.info("save: member ={}", member);
+        log.info("save: member={}", member);
         store.put(member.getId(), member);
         return member;
     }
@@ -24,24 +24,10 @@ public class MemberRepository {
     }
 
     public Optional<Member> findByLoginId(String loginId){
-//        List<Member> all = findAll();
-//        for (Member m : all) {
-//            if(m.getLoginId().equals(loginId)){
-//                return Optional.of(m);
-//            }
-//        }
-//        return Optional.empty();
 
-        return findAll().stream()
-                .filter(m -> m.getLoginId().equals(loginId))
-                .findFirst();
     }
 
     public List<Member> findAll(){
         return new ArrayList<>(store.values());
-    }
-
-    public void clearStore(){
-        store.clear();
     }
 }
